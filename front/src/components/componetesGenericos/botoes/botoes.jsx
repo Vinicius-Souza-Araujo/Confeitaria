75da85d157de3d79e96b167bfb0e38f8c7a5139d
Rayane Novaes <rayanenovaes25@gmail.com>
2023-08-23 18:13:07 -0300

    feat: adicionando tela de login

---
+++ front/src/components/componetesGenericos/botoes/botoes.jsx
@@ -0,0 +1,41 @@
+import "./botoes.css"
+
+export const BotaoRosa = (props) => {
+
+    return(
+        
+                
+            <button className="botaoRosa">{props.texto}</button>     
+        
+    )
+}
+
+export const BotaoAzul = (props) => {
+    return(
+        
+        <button className="botaoAzul">{props.texto}</button>   
+        
+    )
+}
+
+export const BotaoCiano = (props) => {
+    return(
+        
+         <button className="botaoCiano">{props.texto}</button> 
+        
+    )
+}
+
+export const BotaoSummit = (props) => {
+    return(
+        
+            <input className="botaoSummit" type="submit" id={props.id} value={props.texto} />
+    
+    )
+}
+
+
+
+
+
+
