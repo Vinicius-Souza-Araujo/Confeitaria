89b084dce5220a702b05d362b30d96ce09da88fa
Luan Figueredo <luanfigueredodasilva@gmail.com>
2023-10-30 22:24:13 -0300

    add entidade itens_pedido

---
+++ api/src/main/java/com/example/api/domain/entity/ItensPedido.java
@@ -0,0 +1,34 @@
+package com.example.api.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "itens_pedido")
+public class ItensPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedidos pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "subtotal", precision = 10, scale = 2, columnDefinition = "DECIMAL(10, 2) default 0.00")
+    private BigDecimal subtotal;
+
+}
