1c9de5bd9f96a7d8ef9420ab730ce1668ac33366
Marcelo Guimarães <sp.mguimaraes@gmail.com>
2023-11-06 21:36:49 -0300

    Finalizando o Historico do Pedido

--- front/src/components/header/HeaderHome.jsx
+++ front/src/components/header/HeaderHome.jsx
@@ -5,39 +5,46 @@ import { UserContext } from '../../UserContext';
 import './Header.css';
 
 const HeaderHome = () => {
-    const { userLogout } = React.useContext(UserContext);
-    const { data, login } = React.useContext(UserContext);
-    const navigate = useNavigate('');
+  const { userLogout } = React.useContext(UserContext);
+  const { data, login } = React.useContext(UserContext);
+  const navigate = useNavigate('');
 
-    function handleLogout() {
-        userLogout();
-    }
+  function handleLogout() {
+    userLogout();
+  }
 
   return (
     <header className='header'>
       <nav className='nav container'>
-        
-        
+
         <Link className='login'>
-            {data.email}
+          {data.email}
         </Link>
 
         <div>
-            <img src="src\assets\carrinho.png" alt='Carrinho' />
+          <img src="src\assets\carrinho.png" alt='Carrinho' />
 
-            {login && (
-            <button onClick={handleLogout} className='button'>
-                <img src="src\assets\sair.svg" alt='Sair' />
+          {data.grupo === 'CLIENTE' && (
+            <button onClick={() => { navigate('/pedidos') }} className='button'>
+              <img src="src/assets/pedidos.png" alt='Meus pedidos' style={{ width: '25px', height: '25px'}}/>
+              <span className='caption'>Meus pedidos</span>
             </button>
-            )}
-            {data && data.grupo === 'CLIENTE' && (
-              <button className="botao-alterar" onClick={() => navigate('/alterarCliente')}>
-                <img src="src\assets\configuracao.png" alt="Alterar" />
-              </button>
-        )}
-            
+          )}
+
+          {login && (
+            <button onClick={handleLogout} className='button'>
+              <img src="src\assets\sair.svg" alt='Sair' />
+            </button>
+          )}
+
+          {data && data.grupo === 'CLIENTE' && (
+            <button className="botao-alterar" onClick={() => navigate('/alterarCliente')}>
+              <img src="src\assets\configuracao.png" alt="Alterar" />
+            </button>
+          )}
+
         </div>
-        
+
       </nav>
     </header>
   )
