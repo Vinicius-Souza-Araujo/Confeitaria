935a475c1b4f500135c931457b483ae6176fcf05
Marcelo Guimarães <sp.mguimaraes@gmail.com>
2023-12-05 10:34:04 -0300

    Alteração do layout do historico do pedido Filtro para somente produtos ativos na home

--- front/src/components/componetesGenericos/Formulários/FormPedidos/GerenciamentoPedidos.jsx
+++ front/src/components/componetesGenericos/Formulários/FormPedidos/GerenciamentoPedidos.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './GerenciamentoPedidos.css';
-import { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../../../../UserContext';
 import { GET_PEDIDOS } from '../../../../Api';
 
 const GerenciamentoPedidos = () => {
-
     const [pedidos, setPedidos] = useState([]);
     const [filtro, setFiltro] = useState('');
     const user = useContext(UserContext);
@@ -14,67 +12,81 @@ const GerenciamentoPedidos = () => {
 
     useEffect(() => {
         getPedidos();
-        console.log(pedidos)
     }, []);
 
     async function getPedidos() {
         const { url, options } = GET_PEDIDOS(user.data.id);
         const response = await fetch(url, options);
         const pedidos = await response.json();
-        console.log(pedidos);
         setPedidos(pedidos);
     }
 
     const handleBuscar = () => {
         const pedidosFiltrados = pedidos.filter((pedido) =>
-            pedido.numero_pedido.toString().includes(filtro) ||
-            pedido.data_pedido.includes(filtro)
+            pedido.numeroPedido.toString().includes(filtro) ||
+            pedido.dataPedido.includes(filtro)
         );
         setPedidos(pedidosFiltrados);
-    }
+    };
 
     const toggleDetalhes = (pedido) => {
         if (detalhesPedido === pedido) {
-            setDetalhesPedido(null); // Fecha os detalhes se já estiverem abertos.
+            setDetalhesPedido(null);
         } else {
-            setDetalhesPedido(pedido); // Abre os detalhes.
+            setDetalhesPedido(pedido);
         }
-    }
+    };
 
     return (
-
         <div id="gerenciamento-pedidos" className="gerenciamento-pedidos">
-
             <h1 className='titulo-principal'>Meus Pedidos</h1>
             <h2 className='nome-cliente'>Cliente: {data.email}</h2>
 
             <label className='box-input-buscar'>
                 <input onChange={(event) => setFiltro(event.target.value)} className='input-buscar' placeholder='Buscar...' />
-                <button className='botaoAzul' onClick={handleBuscar}>Buscar</button>
+                <button className='botaoSummit' onClick={handleBuscar}>Buscar</button>
             </label>
 
-            {pedidos.map((pedido, index) => (
-                <div className="pedido-card" key={index}>
-                    <div className="pedido-info">
-                        <p>Número do pedido: {pedido.numeroPedido}</p>
-                        <p>Data: {pedido.dataPedido}</p>
-                        <p>Valor total: {pedido.valorTotal}</p>
+            <div className='pedido-cards-container'>
+                {pedidos.map((pedido, index) => (
+                    <div className="pedido-card" key={index}>
 
-                        <button onClick={() => toggleDetalhes(pedido)}>Detalhes</button>
-                        <div>
-                            {detalhesPedido === pedido && (
-                                <div className="pedido-detalhes">
-                                    <p>Status: {pedido.statusPedido}</p>
-                                    <p>Endereço de entrega:</p>
-                                    <p>Forma de pagamento:</p>
-                                </div>
-                            )}
+                        <div className="pedido-info">
+                            <p>Número do pedido: {pedido.numeroPedido}</p>
+                            <p>Data: {pedido.dataPedido}</p>
+                            <p>Valor total: {pedido.valorTotal}</p>
+                            <button className='botaoAzul' onClick={() => toggleDetalhes(pedido)}>Detalhes</button>
                         </div>
-                    </div>
-                </div>
-            ))}
-        </div>
 
+                        {detalhesPedido === pedido && (
+                            <div className="pedido-detalhes">
+                                <div>
+                                    <li>Status</li>
+                                    {pedido.statusPedido}
+                                    <p>-------------------------------------------------</p>
+                                </div>
+                                <div>
+                                    <li>Endereço de entrega</li>
+                                    {pedido.statusPedido}
+                                    <p>-------------------------------------------------</p>
+                                </div>
+                                <div>
+                                    <li>Forma de pagamento</li>
+                                    {pedido.formaPagamento}
+                                    <p>-------------------------------------------------</p>
+                                </div>
+                                <div>
+                                    <li>Frete</li>
+                                    {pedido.frete}
+                                    <p>-------------------------------------------------</p>
+                                </div>
+                            </div>
+                        )}
+
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
